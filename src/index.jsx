90149ac1985bd6049f17382d330d54661d822536
franciscogelabert <francisco.gelabert@gmail.com>
2023-09-19 21:10:00 -0300

    Se se agregan variables de entorno y guardado de pedido

--- src/index.jsx
+++ src/index.jsx
@@ -5,19 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 
-
-// Your web app's Firebase configuration
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDbXWfzgCumsW44rNCvaWyu8Tud7RbfVag",
-  authDomain: "foodierreact.firebaseapp.com",
-  projectId: "foodierreact",
-  storageBucket: "foodierreact.appspot.com",
-  messagingSenderId: "996735585014",
-  appId: "1:996735585014:web:1c738ba75f0850c718fad7"
-};*/
-
-
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIRESTORE_API_KEY,
   authDomain: import.meta.env.VITE_FIRESTORE_AUTH_DOMAIN,
@@ -29,9 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
